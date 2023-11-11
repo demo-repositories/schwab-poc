252c26e3d3f02a5d6c7bf235f792028f29a741d0
ChrisLaRocque <larocque.christopher@gmail.com>
2023-11-10 22:01:58 -0500

    Update next.config.js
    Wrong attribute

--- next/next.config.js
+++ next/next.config.js
@@ -6,9 +6,7 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
+    ignoreBuildErrors: true,
   },
 };
 
