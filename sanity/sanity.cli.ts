dbae604de8eb723a375c06c8429788cd7f8c272e
ChrisLaRocque <larocque.christopher@gmail.com>
2023-12-08 18:06:50 -0500

    More auth token cleanup

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -2,7 +2,7 @@ import { defineCliConfig } from 'sanity/cli'
 // Config file used by the Sanity CLI https://www.sanity.io/docs/cli#4baf8325e0e3
 export default defineCliConfig({
     api: {
-        projectId: process.env.SANITY_PROJECT_ID,
+        projectId: process.env.SANITY_PROJECT_ID || 'fvuvea00',
         dataset: 'production',
     },
 })
